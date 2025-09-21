d4be218d557135ca47597eacad54c2dba562cf64
Ticasslo <win2005thang@gmail.com>
2025-09-21 15:50:18 +0700

    Khoi tao du an

---
+++ 23110327_HuynhNgocThang_Nhom16_CodeQuanLyThuChiTaiChinh/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23110327_HuynhNgocThang_Nhom16_CodeQuanLyThuChiTaiChinh
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
